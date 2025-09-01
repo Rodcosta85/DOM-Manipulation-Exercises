4f582ac49e70eddd1e21c4892a633e8faf70c6fc
Rodcosta85 <147288678+Rodcosta85@users.noreply.github.com>
2025-09-01 18:58:51 -0300

    exercise 15 half done

--- src/Exercise 15/ExerciseFifteen.tsx
+++ src/Exercise 15/ExerciseFifteen.tsx
@@ -1,22 +1,44 @@
-import React from 'react'
+import { useState } from 'react'
 
 function ExerciseFifteen() {
+
+    const [input, setInput] = useState('');
+    const [currencies] = useState<string[]>([
+        'Brasil(R$)',
+        'USA($)',
+        'Europe(€)',
+        'Russia(₽)',
+        'Japan(¥)',
+        'UK(£)'
+    ]);
+
+    const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInput(e.target.value);
+    }
+
+
+
     return (
-        <div className='flex flex-col gap-3 border-[1.5px] border-gray-300 rounded-[6px] p-4'>
+        <div className='col-span-2 flex flex-col gap-3 border-[1.5px] border-gray-300 rounded-[6px] p-4'>
             <h1 className="font-semibold text-white">Exercise 15</h1>
-            <p className="text-white text-[14px]">Input a number and a currency dropdown; on change, convert the value to another currency
+            <p
+                className="text-white text-[14px]">Input a number and a currency dropdown; on change, convert the value to another currency
                 using static rates.</p>
-            <div className='flex flex-col gap-2'>
-                <input type="text" placeholder='insert your value here'/>
-                <ul className='flex flex-col gap-2 bg-red-300'>
-                    <li className='text-white text-[12px] cursor-pointer'>Reais</li>
-                    <li className='text-white text-[12px] cursor-pointer'>US Dollars</li>
-                    <li className='text-white text-[12px] cursor-pointer'>Pounds</li>
-                    <li className='text-white text-[12px] cursor-pointer'>Euros</li>
-                    <li className='text-white text-[12px] cursor-pointer'>Yen</li>
-                    <li className='text-white text-[12px] cursor-pointer'>Renminbi</li>
-                </ul>
+            <div className='flex flex-row gap-10 w-full'>
+                <input
+                    value={input}
+                    onChange={handleInput}
+                    type="number"
+                    placeholder='insert your value here' 
+                    className='focus:outline-none text-white border-b-white border-b-[1px]'
+                    />
+                <select name="" id="" className='focus:outline-none'>
+                    {currencies.map((currency) => (
+                        <option key={currency} value="">{currency}</option>
+                    ))}
+                </select>
             </div>
+            <p><b>RESULTADO:</b></p>
         </div>
     )
 }
